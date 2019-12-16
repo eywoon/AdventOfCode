14e16020a0675c23c97eafe622f55e0d188ea5f8
eywoon <eyrunthr@gmail.com>
2019-12-16 23:37:09 +0100

    dec2 skeleton

--- src/main/java/Dec2.java
+++ src/main/java/Dec2.java
@@ -1,7 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dec2 {
 
 
-    public static void main(String args){
+    enum opcodeType{
+       ONE, TWO, NINTYNINE
+    }
+    //process opcode (4 positions)
+
+    //opcode1 calculation -> add and overwrite
+    public static Map<Integer, Integer>  opcode1(int value1Index, int value2Index, int overwriteIndex, Map<Integer, Integer> program){
+
+        int val1 = program.get(value1Index);
+        int val2 = program.get(value2Index);
+
+        program.put(overwriteIndex, val1+val2);
+
+        return program;
+    }
+
+    //opcode2 calculation -> multiply and overwrite
+    public static Map<Integer, Integer>  opcode2(int value1Index, int value2Index, int overwriteIndex,  Map<Integer, Integer> program){
+        int val1 = program.get(value1Index);
+        int val2 = program.get(value2Index);
+
+        program.put(overwriteIndex, val1*val2);
+
+        return program;
+    }
+
+    public void whatOpcode(opcodeType opcode){
+        switch(opcode) {
+            case ONE:
+                System.out.println("addition");
+                break;
+            case TWO:
+                System.out.println("multiplycation");
+                break;
+            case NINTYNINE:
+                System.out.println("end program");
+                break;
+        }
+    }
+
+    public static void main(String[] args){
+
+        int[] a = {1,9,10,3,2,3,11,0,99,30,40,50};
+        HashMap<Integer, Integer> program = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < a.length; i++){
+            program.put(i, a[i]);
+        }
+
+        //
+        opcode1(0,1,3, program);
+
+        System.out.println(program.get(3));
 
     }
 }
